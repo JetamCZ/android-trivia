4aa47ed1a14368c39e3c0e5a15f266c6f4783374
Matej Puhony <matej.puhony@seznam.cz>
2024-11-29 09:51:02 +0100

    Add some basic texts

--- app/src/main/java/eu/puhony/trivia/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/eu/puhony/trivia/ui/screens/login/LoginScreen.kt
@@ -1,11 +1,16 @@
 package eu.puhony.trivia.ui.screens.login
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -13,9 +18,30 @@ fun LoginScreen(
     modifier: Modifier = Modifier,
     viewModel: LoginScreenModel = viewModel(),
 ) {
+    Column(modifier = modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 20.dp)) {
+        Text(
+            text ="Welcome in Trivia app",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+        )
 
-    Column(modifier) {
+        Text(
+            text ="Select already existing user",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 40.dp)
+        )
+
+        Text(
+            text ="Or login as a new user...",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 40.dp)
+        )
+
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = viewModel.username,
             label = {
                 Text(text = "Username")
@@ -24,5 +50,12 @@ fun LoginScreen(
                 viewModel.setUserName(it)
             }
         )
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Continue as ${viewModel.username}")
+        }
     }
 }
